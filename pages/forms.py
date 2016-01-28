f8d0c847e3b94e3af4abb9202ebf31c7a9396f33
Light07 <kevin.cai@outlook.com>
2016-01-28 11:56:15 +0800

    1. Add gif to guide user to input. 2. Add loading page while waiting. 3. Add note to explain reason of generate report is slow.

--- pages/forms.py
+++ pages/forms.py
@@ -4,18 +4,20 @@ from flask_wtf import Form
 from wtforms import StringField, IntegerField, SubmitField, PasswordField, validators, HiddenField, SelectField
 
 class EFForm(Form):
+
     qa_resource = StringField(u'QA Resource', [validators.DataRequired("Please input the qa who contributed on this sprint.")])
-    username = StringField(u'User Name(for jira)', [validators.DataRequired("Please input the user name that used for jira login.")])
-    password = PasswordField(u'Password', [validators.DataRequired("Please input the password.")])
+    username = StringField(u'Jira Username', [validators.DataRequired("Please input the user name that used for jira login.")])
+    password = PasswordField(u'Jira Password', [validators.DataRequired("Please input the password.")])
     project_name = StringField(u'Project Name', [validators.DataRequired("Please input the project name")])
     board_id = IntegerField(u'Board Id', [validators.DataRequired("Please input a integer as board id.")])
-    submit = SubmitField(u'Generate Report')
+    submit = SubmitField(u'Submit')
 
 class SelectFieldForm(Form):
-        qa_resource = HiddenField(StringField(u'QA Resource'), [validators.DataRequired("Please input the qa who contributed on this sprint.")])
-        username = HiddenField(StringField(u'User Name(for jira)'), [validators.DataRequired("Please input the user name that used for jira login.")])
-        password = HiddenField(PasswordField(u'Password'), [validators.DataRequired("Please input the password.")])
-        project_name = HiddenField(StringField(u'Project Name'), [validators.DataRequired("Please input the project name")])
-        sprint_name = SelectField(u'Sprint Name', coerce=str)
-        board_id = HiddenField(IntegerField(u'Board Id'), [validators.DataRequired("Please input a integer as board id.")])
-        submit = SubmitField(u'Generate Report')
+
+    qa_resource = HiddenField(StringField(u'QA Resource'), [validators.DataRequired("Please input the qa who contributed on this sprint.")])
+    username = HiddenField(StringField(u'Jira Username'), [validators.DataRequired("Please input the user name that used for jira login.")])
+    password = HiddenField(PasswordField(u'Jira Password'), [validators.DataRequired("Please input the password.")])
+    project_name = HiddenField(StringField(u'Project Name'), [validators.DataRequired("Please input the project name")])
+    sprint_name = SelectField(u'Sprint Name', coerce=str)
+    board_id = HiddenField(IntegerField(u'Board Id'), [validators.DataRequired("Please input a integer as board id.")])
+    submit = SubmitField(u'Submit')
