78c5ac8e12e74baaa4aa78b51212358bdc92afbc
Light07 <kevin.cai@outlook.com>
2016-02-17 16:58:11 +0800

    Fix bug of calculate bug trends.

--- pages/jira_helper.py
+++ pages/jira_helper.py
@@ -3,7 +3,7 @@
 # Why jira API is slow, check out:
 # https://jira.atlassian.com/browse/JRA-36224
 # https://jira.atlassian.com/browse/JRA-30170
-
+from collections import defaultdict
 from DateTime import DateTime
 
 from jira.client import JIRA
@@ -213,6 +213,60 @@ class JiraHelper(object):
         else:
             return None
 
+    def get_task_status_change_date(self, id):
+        '''
+        :param id:
+        :return:  [('2016-01-13', 'Created'), ('2016-01-14', 'Created'), ('2016-01-15', 'Created'), ('2016-01-16', 'Created'), ('2016-01-17', 'Created'), ('2016-01-18', 'Created'), ('2016-01-19', 'Created'), ('2016-01-20', 'Created'), ('2016-01-21', 'Created'), ('2016-01-22', 'Created'), ('2016-01-23', 'Created'), ('2016-01-24', 'Created'), ('2016-01-25', 'Created'), ('2016-01-26', 'Created'), ('2016-01-27', 'Created'), ('2016-01-28', 'Created'), ('2016-01-29', 'Created'), ('2016-01-30', 'Created'), ('2016-01-31', 'Created'), ('2016-02-01', 'Created'), ('2016-02-02', 'Created'), ('2016-02-03', 'Created'), ('2016-02-04', 'Created')]
+
+        '''
+        return_dict = {}
+        temp_dict = {}
+        raw_status_change_dict = {}
+        issue = self.jira.issue(id, expand='changelog')
+        change_log = issue.changelog
+        for history in change_log.histories:
+            for item in history.items:
+                if item.field == 'status':
+                    if item.toString == 'Resolved':
+                        raw_status_change_dict[DateTime(history.created)] = "Resolved"
+                    if item.toString == 'Closed':
+                        raw_status_change_dict[DateTime(history.created)] = "Closed"
+                    if item.toString == 'Reopened':
+                        raw_status_change_dict[DateTime(history.created)] = "Open"
+        # raw_status_change_dict[DateTime(issue.fields.created)] = "Created"
+
+        ## Get the sorted date with status.
+        for k in raw_status_change_dict.keys():
+            str_k = str(k.asdatetime().strftime("%Y-%m-%d"))
+            if str_k not in temp_dict.keys():
+                return_dict[str_k] = raw_status_change_dict[k]
+                temp_dict[str_k] = k
+            else:
+                if k > temp_dict[str_k]:
+                    return_dict[str_k] = raw_status_change_dict[k]
+                else:
+                    return_dict[str_k] = raw_status_change_dict[temp_dict[str_k]]
+
+        return_dict[DateTime(issue.fields.created).asdatetime().strftime("%Y-%m-%d")] = "Created"
+        # return_dict["id"] = id
+        return_list= sorted(return_dict.iteritems(), key=lambda d:d[0])
+
+        #Get the sorted status till end of call day.
+        final_dict = {}
+        i=0
+        if len(return_list) > 1:
+            while i < len(return_list)-1:
+                start =  DateTime(str(return_list[i][0]))
+                end = DateTime(str(return_list[i+1][0]))
+                while start <= end:
+                    final_dict[start.asdatetime().strftime("%Y-%m-%d")] = return_list[i][1]
+                    start +=1
+                i +=1
+        else:
+            final_dict[return_list[0][0]] = return_list[0][1]
+        return final_dict #sorted(final_dict.iteritems(), key=lambda d:d[0]) #
+
+
     def get_closed_task_num_group_by_date(self, j_query):
         '''
         return the  date number pair with given jquery.
@@ -265,70 +319,133 @@ class JiraHelper(object):
 
         return len(issue_ids), len(issue_ids) - closed_number, closed_number, resoved_number, len(issue_ids) - closed_number - resoved_number
 
+    # def html_get_total_bug_and_open_bug_trend_by_sprint(self, sprint_id, id_of_board=config.board_id, project=config.project_name, component_filter=None):
+    #     '''
+    #     :param id_of_board:
+    #     :param sprint_id:
+    #     :return: [
+    #       ['Date',  'total bug number', 'Opened bug number'],
+    #       ['2015-12-07', 0, 0], ['2015-12-08', 0, 0], ['2015-12-09', 0, 1], ['2015-12-10', 2, 3], ['2015-12-11', 3, 5], ['2015-12-12', 4, 6]
+    #       ]
+    #     '''
+    #     # Version1 and Version2, bug trends should use this block.
+    #         # sprint_info = self.get_sprint_info(sprint_id, id_of_board)
+    #         #
+    #         # start_date = DateTime(sprint_info["startDate"])
+    #         # end_date = DateTime(sprint_info["endDate"])
+    #         # current_date = DateTime(start_date)
+    #         # nested_list_for_html = []
+    #         # title_list = ["date", "total bug number", "opened bug number"]
+    #         # nested_list_for_html.append(title_list)
+    #         #
+    #         # while current_date <= end_date:
+    #         #     j_query_string = '''project = {project} AND issuetype in ({issue_type}) and created >= {start_day} AND created <= {end_day}'''\
+    #         #         .format(project= project, issue_type= self.IssueType.Bug, start_day=start_date.asdatetime().strftime("%Y-%m-%d"), end_day=current_date.asdatetime().strftime("%Y-%m-%d"))
+    #         #     temp_list = []
+    #         #     temp_list.append(current_date.asdatetime().strftime("%Y-%m-%d"))
+    #         #
+    #         #     bug_number_info = self.get_total_bug_and_open_bug_num_by_time_period(j_query_string, current_date)
+    #         #     total_number = bug_number_info[0]
+    #         #     opened_number = bug_number_info[1]
+    #         #     temp_list.append(total_number)
+    #         #     temp_list.append(opened_number)
+    #         #     nested_list_for_html.append(temp_list)
+    #         #     current_date = current_date +1
+    #         #
+    #         # return nested_list_for_html
+    #
+    #     # Version 3, current use
+    #     sprint_info = self.get_sprint_info(sprint_id, id_of_board)
+    #     start_date = DateTime(sprint_info["startDate"])
+    #     end_date = DateTime(sprint_info["endDate"])
+    #     current_date = DateTime(start_date)
+    #     nested_list_for_html = []
+    #     title_list = ["date", "closed bugs", "resolved bugs", "open bugs"]
+    #     nested_list_for_html.append(title_list)
+    #
+    #     while current_date <= end_date:
+    #         j_query_string = '''project = {project} AND issuetype in ({issue_type}) and created >= {start_day} AND created <= {end_day}'''\
+    #             .format(project= project, issue_type= self.IssueType.Bug, start_day=start_date.asdatetime().strftime("%Y-%m-%d"), end_day=current_date.asdatetime().strftime("%Y-%m-%d"))
+    #         temp_list = []
+    #         temp_list.append(current_date.asdatetime().strftime("%Y-%m-%d"))
+    #
+    #         if component_filter:
+    #             j_query_string = j_query_string + ''' and component in ({component})'''.format(component=component_filter)
+    #         bug_number_info = self.get_total_bug_and_closed_open_bug_num_by_time_period(j_query_string, current_date)
+    #         closed_number = bug_number_info[2]
+    #         resolved_number = bug_number_info[3]
+    #         opened_number = bug_number_info[4]
+    #         temp_list.append(closed_number)
+    #         temp_list.append(resolved_number)
+    #         temp_list.append(opened_number)
+    #         nested_list_for_html.append(temp_list)
+    #         current_date = current_date +1
+    #
+    #     return nested_list_for_html
+
     def html_get_total_bug_and_open_bug_trend_by_sprint(self, sprint_id, id_of_board=config.board_id, project=config.project_name, component_filter=None):
         '''
         :param id_of_board:
         :param sprint_id:
         :return: [
-          ['Date',  'total bug number', 'Opened bug number'],
-          ['2015-12-07', 0, 0], ['2015-12-08', 0, 0], ['2015-12-09', 0, 1], ['2015-12-10', 2, 3], ['2015-12-11', 3, 5], ['2015-12-12', 4, 6]
+          ["date", "closed bugs", "resolved bugs", "open bugs"],
+          ['2015-12-07', 0, 0, 0], ['2015-12-08', 0, 0, 0], ['2015-12-09', 0, 1, 1]
           ]
         '''
-        # Version1 and Version2, bug trends should use this block.
-            # sprint_info = self.get_sprint_info(sprint_id, id_of_board)
-            #
-            # start_date = DateTime(sprint_info["startDate"])
-            # end_date = DateTime(sprint_info["endDate"])
-            # current_date = DateTime(start_date)
-            # nested_list_for_html = []
-            # title_list = ["date", "total bug number", "opened bug number"]
-            # nested_list_for_html.append(title_list)
-            #
-            # while current_date <= end_date:
-            #     j_query_string = '''project = {project} AND issuetype in ({issue_type}) and created >= {start_day} AND created <= {end_day}'''\
-            #         .format(project= project, issue_type= self.IssueType.Bug, start_day=start_date.asdatetime().strftime("%Y-%m-%d"), end_day=current_date.asdatetime().strftime("%Y-%m-%d"))
-            #     temp_list = []
-            #     temp_list.append(current_date.asdatetime().strftime("%Y-%m-%d"))
-            #
-            #     bug_number_info = self.get_total_bug_and_open_bug_num_by_time_period(j_query_string, current_date)
-            #     total_number = bug_number_info[0]
-            #     opened_number = bug_number_info[1]
-            #     temp_list.append(total_number)
-            #     temp_list.append(opened_number)
-            #     nested_list_for_html.append(temp_list)
-            #     current_date = current_date +1
-            #
-            # return nested_list_for_html
-
-        # Version 3, current use
         sprint_info = self.get_sprint_info(sprint_id, id_of_board)
-        start_date = DateTime(sprint_info["startDate"])
-        end_date = DateTime(sprint_info["endDate"])
-        current_date = DateTime(start_date)
+        start_date = DateTime(str(sprint_info["startDate"]) + ' ' +  "US/Eastern")
+        end_date = DateTime(str(sprint_info["endDate"]) + ' ' +  "US/Eastern")
+
         nested_list_for_html = []
         title_list = ["date", "closed bugs", "resolved bugs", "open bugs"]
         nested_list_for_html.append(title_list)
 
-        while current_date <= end_date:
-            j_query_string = '''project = {project} AND issuetype in ({issue_type}) and created >= {start_day} AND created <= {end_day}'''\
-                .format(project= project, issue_type= self.IssueType.Bug, start_day=start_date.asdatetime().strftime("%Y-%m-%d"), end_day=current_date.asdatetime().strftime("%Y-%m-%d"))
-            temp_list = []
-            temp_list.append(current_date.asdatetime().strftime("%Y-%m-%d"))
+        j_query_string = '''project = {project} AND issuetype in ({issue_type}) and created >= {start_day} AND created <= {end_day}'''\
+                .format(project= project, issue_type= self.IssueType.Bug, start_day=start_date.asdatetime().strftime("%Y-%m-%d"), end_day=end_date.asdatetime().strftime("%Y-%m-%d"))
 
-            if component_filter:
-                j_query_string = j_query_string + ''' and component in ({component})'''.format(component=component_filter)
-            bug_number_info = self.get_total_bug_and_closed_open_bug_num_by_time_period(j_query_string, current_date)
-            closed_number = bug_number_info[2]
-            resolved_number = bug_number_info[3]
-            opened_number = bug_number_info[4]
-            temp_list.append(closed_number)
-            temp_list.append(resolved_number)
-            temp_list.append(opened_number)
-            nested_list_for_html.append(temp_list)
-            current_date = current_date +1
+        if component_filter:
+            j_query_string = j_query_string + ''' and component in ({component})'''.format(component=component_filter)
+
+        all_ids = self.get_task_id_by_query_string(j_query_string)
+        task_status_change_date_list = []
+        for id in all_ids:
+            data_status = self.get_task_status_change_date(str(id))
+
+            sorted_data = sorted(data_status.iteritems(), key=lambda d:d[0])
+
+            time_start = start_date
+            while time_start <= end_date:
+                str_start_date = time_start.asdatetime().strftime("%Y-%m-%d")
+                if len(data_status) >=1:
+                    if DateTime(str_start_date) < DateTime(sorted_data[0][0]):
+                        data_status[str_start_date] = "NotOpen"
+
+                    if DateTime(str_start_date) > DateTime(sorted_data[len(sorted_data)-1][0]):
+                        data_status[str_start_date] = sorted_data[len(sorted_data)-1][1]
+
+                time_start = DateTime(str_start_date)
+                time_start  = time_start +1
+
+            for k in data_status.keys():
+                if DateTime(k) > DateTime(end_date.asdatetime().strftime("%Y-%m-%d")):
+                    del data_status[k]
+
+            task_status_change_date_list.append(data_status)
+
+        status_dict = {}
+        for d in task_status_change_date_list:
+            for k, v in d.iteritems():
+                status_dict.setdefault(k, []).append(v)
+
+        for item in  sorted(status_dict.iteritems(), key=lambda d:d[0]):
+            single_list = []
+            single_list.append(item[0])
+            single_list.append(item[1].count("Closed"))
+            single_list.append(item[1].count("Resolved"))
+            single_list.append(item[1].count("Created") + item[1].count("Open"))
+            nested_list_for_html.append(single_list)
 
         return nested_list_for_html
-
 
     def get_task_info_by_id(self, id):
         issue = self.jira.issue(id)
@@ -371,7 +488,7 @@ class JiraHelper(object):
     def get_task_id_by_query_string(self, j_query):
         issue_list = []
         for item in self.jira.search_issues(j_query, maxResults=False):
-            issue_list.append(item)
+            issue_list.append(str(item))
         return issue_list
 
     def get_automation_found_bug_info_by_sprint(self, sprint_id, id_of_board_id=config.board_id, project=config.project_name):
@@ -783,6 +900,10 @@ class JiraHelper(object):
                 status = "fail"
         return status
 
-# if __name__ == "__main__":
-    # jira = JiraHelper(config.jira_options, config.jira_account)
+if __name__ == "__main__":
+    jira = JiraHelper(config.jira_options, config.jira_account)
+    # print jira.get_task_info_by_id("ATEAM-4111")
+    # print jira.get_task_status_change_date("ATEAM-4096")
+    # print jira.get_sprint_info(1858, 60)
     # print jira.get_issue_resolved_status_by_date('ME-2628', '2016-02-02')
+    print jira.html_get_total_bug_and_open_bug_trend_by_sprint(1858, 60, "ATEAM")
